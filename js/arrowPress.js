7a969bd8abb03ffe4fd08a2242e31e54fefa966e
Raymond Lam <z5255290@unsw.edu.au>
2020-05-12 18:08:17 +1000

    fixed funcionality and bugs

--- js/arrowPress.js
+++ js/arrowPress.js
@@ -1,9 +1,12 @@
 const suggest = document.querySelector('.suggestions');
 let selections = 0;
+let hasSelection = false;
 window.addEventListener('keyup', e => {
     if (e.keyCode == 40) {
+        hasSelection = true;
         arrowDown();
     } else if (e.keyCode == 38) {
+        hasSelection = true;
         arrowUp();
     } else {
         selections = 0;
@@ -11,38 +14,43 @@ window.addEventListener('keyup', e => {
     }
 })
 
-
-
 function arrowDown() {
     suggest.scrollTo(0, selections * 40);
-    
     children = suggest.childNodes;
     if (children.length == 0) {
         selections = 0;
+        hasSelection = false;
         return;
     }
+    console.log(selections);
+    console.log(children.length);
+    if (selections > 0)
+        children[selections - 1].className = 'selected'
     if (selections < children.length) {
         children[selections].className = 'selected';
         children[selections].focus();
         if (selections > 0 && selections != children.length - 1) {
             children[selections - 1].className = 'suggest';
         }
-        if (selections != children.length - 1)
+        if (selections != children.length)
             selections++;
     }
 }
 
 
 function arrowUp() {
-    let suggests = document.querySelector('.suggestions');
-    suggests.scrollTo(0, selections * 40);
-    children = suggests.childNodes;
+    suggest.scrollTo(0, selections * 40);
+    children = suggest.childNodes;
     console.log(selections);
+    console.log(children.length)
     if (children.length == 0) {
         selections = 0;
+        hasSelection = false;
         return;
     }
-    if (selections < children.length) {
+    if (selections == children.length)
+        selections--;
+    if (selections >= 0) {
         children[selections].className = 'selected';
         children[selections].focus();
         if (selections < children.length - 1)
@@ -50,5 +58,4 @@ function arrowUp() {
         if (selections != 0)
             selections--;
     }
-
 }
